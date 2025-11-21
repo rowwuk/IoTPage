e5479d487b3b12a33d87dc07e52a155ba5c75feb
rowwuk <zawnox@gmail.com>
2025-11-21 13:16:45 +0100

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -43,7 +43,7 @@ export default function Home() {
           >
             <Image
               className="dark:invert"
-              src="/vercel.svg"
+              src="/IoTPage/vercel.svg"
               alt="Vercel logomark"
               width={16}
               height={16}
