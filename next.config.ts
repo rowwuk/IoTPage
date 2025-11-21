e1a3afd4c1976e27fb901e5c4fa3d1450d9278cc
rowwuk <zawnox@gmail.com>
2025-11-21 12:53:39 +0100

    github pages setup

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  basePath: "/IoTPage",
+  output: "export",
+  reactStrictMode: true,
 };
 
 export default nextConfig;
